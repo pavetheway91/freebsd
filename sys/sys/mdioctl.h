cd669b7f5d1f47b900aa41c68812e2e523095178
Paavo-Einari Kaipila <pkaipila@gmail.com>
2019-06-19 15:25:45 +0300

    Panic fixed, adding mdioctl.h that I forgot from an earlier commit

--- sys/sys/mdioctl.h
+++ sys/sys/mdioctl.h
@@ -45,7 +45,7 @@
 #ifndef _SYS_MDIOCTL_H_
 #define _SYS_MDIOCTL_H_
 
-enum md_types {MD_MALLOC, MD_PRELOAD, MD_VNODE, MD_SWAP, MD_NULL};
+enum md_types {MD_MALLOC, MD_COMPRESSED, MD_PRELOAD, MD_VNODE, MD_SWAP, MD_NULL};
 
 /*
  * Ioctl definitions for memory disk pseudo-device.
